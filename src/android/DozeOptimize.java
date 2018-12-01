e299b1591f2a37e27bc330c44f1bd6b93e64d865
Angel Todorov <angel.a.todorov@gmail.com>
2018-12-01 18:44:54 +0200

    Added "startActivityForResult" for "RequestOptimizations" method

--- src/android/DozeOptimize.java
+++ src/android/DozeOptimize.java
@@ -13,9 +13,6 @@ import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
 
-
-
-
 import android.os.Build;
 import android.os.Environment;
 import android.util.Base64;
@@ -31,13 +28,18 @@ import static android.net.ConnectivityManager.RESTRICT_BACKGROUND_STATUS_DISABLE
 import static android.net.ConnectivityManager.RESTRICT_BACKGROUND_STATUS_ENABLED;
 import static android.net.ConnectivityManager.RESTRICT_BACKGROUND_STATUS_WHITELISTED;
 
+import android.widget.Toast;// ToToast
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class DozeOptimize extends CordovaPlugin {
+    private static final int REQUEST_BATTERY_OPTIMIZATIONS = 1;
+    private CallbackContext PUBLIC_CALLBACKS = null;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        PUBLIC_CALLBACKS = callbackContext;
 
         Context context = cordova.getActivity().getApplicationContext();
         String packageName = context.getPackageName();
@@ -45,22 +47,18 @@ public class DozeOptimize extends CordovaPlugin {
         if (action.equals("IsIgnoringBatteryOptimizations")) {
             try {
 
-                if (Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP_MR1) {
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
 
-                    String message ="";
+                    String message = "";
                     PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
                     if (pm.isIgnoringBatteryOptimizations(packageName)) {
-                        message ="true";
-                    }
-                    else
-                    {
-                        message ="false";
+                        message = "true";
+                    } else {
+                        message = "false";
                     }
                     callbackContext.success(message);
                     return true;
-                }
-                else
-                {
+                } else {
                     callbackContext.error("BATTERY_OPTIMIZATIONS Not available.");
                     return false;
                 }
@@ -68,24 +66,25 @@ public class DozeOptimize extends CordovaPlugin {
                 callbackContext.error("IsIgnoringBatteryOptimizations: failed N/A");
                 return false;
             }
-        }else if (action.equals("RequestOptimizations")) {
+        } else if (action.equals("RequestOptimizations")) {
             try {
 
-                if (Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP_MR1) {
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
 
-                    String message ="Optimizations Requested Successfully";
+                    String message = "Optimizations Requested Successfully";
 
                     Intent intent = new Intent();
                     intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    // intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     intent.setData(Uri.parse("package:" + packageName));
-                        context.startActivity(intent);
+                    cordova.setActivityResultCallback(this);
+                    cordova.startActivityForResult((CordovaPlugin) this, intent, REQUEST_BATTERY_OPTIMIZATIONS);
 
-                    callbackContext.success(message);
+                    // Send no result, to execute the callbacks later
+                    PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+                    pluginResult.setKeepCallback(true); // Keep callback
                     return true;
-                }
-                else
-                {
+                } else {
                     callbackContext.error("BATTERY_OPTIMIZATIONS Not available.");
                     return false;
                 }
@@ -100,7 +99,7 @@ public class DozeOptimize extends CordovaPlugin {
 
                     Intent intent = new Intent();
                     PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-                    if (pm.isIgnoringBatteryOptimizations(packageName)){
+                    if (pm.isIgnoringBatteryOptimizations(packageName)) {
                         intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         context.startActivity(intent);
@@ -108,9 +107,7 @@ public class DozeOptimize extends CordovaPlugin {
 
                     callbackContext.success("requested");
                     return true;
-                }
-                else
-                {
+                } else {
                     callbackContext.error("BATTERY_OPTIMIZATIONS Not available.");
                     return false;
                 }
@@ -118,36 +115,34 @@ public class DozeOptimize extends CordovaPlugin {
                 callbackContext.error("RequestOptimizationsMenu: failed N/A");
                 return false;
             }
-        }else if(action.equals("IsIgnoringDataSaver")) {
-             try {
+        } else if (action.equals("IsIgnoringDataSaver")) {
+            try {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
 
-                    String message ="";
-                    ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                    String message = "";
+                    ConnectivityManager connMgr = (ConnectivityManager) context
+                            .getSystemService(Context.CONNECTIVITY_SERVICE);
 
-                    switch (connMgr.getRestrictBackgroundStatus())
-                    {
-                        case RESTRICT_BACKGROUND_STATUS_ENABLED:
-                            // The app is whitelisted. Wherever possible,
-                            // the app should use less data in the foreground and background.
-                            message = "false";
-                            break;
+                    switch (connMgr.getRestrictBackgroundStatus()) {
+                    case RESTRICT_BACKGROUND_STATUS_ENABLED:
+                        // The app is whitelisted. Wherever possible,
+                        // the app should use less data in the foreground and background.
+                        message = "false";
+                        break;
 
-                        case RESTRICT_BACKGROUND_STATUS_WHITELISTED:
-                            // Background data usage is blocked for this app. Wherever possible,
-                            // the app should also use less data in the foreground.
-                        case RESTRICT_BACKGROUND_STATUS_DISABLED:
-                            // Data Saver is disabled. Since the device is connected to a
-                            // metered network, the app should use less data wherever possible.
-                             message = "true";
-                            break;
+                    case RESTRICT_BACKGROUND_STATUS_WHITELISTED:
+                        // Background data usage is blocked for this app. Wherever possible,
+                        // the app should also use less data in the foreground.
+                    case RESTRICT_BACKGROUND_STATUS_DISABLED:
+                        // Data Saver is disabled. Since the device is connected to a
+                        // metered network, the app should use less data wherever possible.
+                        message = "true";
+                        break;
                     }
                     callbackContext.success(message);
                     return true;
 
-                }
-                else
-                {
+                } else {
                     callbackContext.error("DATA_SAVER Not available.");
                     return false;
                 }
@@ -155,11 +150,12 @@ public class DozeOptimize extends CordovaPlugin {
                 callbackContext.error("IsIgnoringDataSaver: failed N/A");
                 return false;
             }
-        }else  if (action.equals("RequestDataSaverMenu")) {
+        } else if (action.equals("RequestDataSaverMenu")) {
             try {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                     Intent intent = new Intent();
-                    ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                    ConnectivityManager connMgr = (ConnectivityManager) context
+                            .getSystemService(Context.CONNECTIVITY_SERVICE);
 
                     intent.setAction(Settings.ACTION_IGNORE_BACKGROUND_DATA_RESTRICTIONS_SETTINGS);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -168,7 +164,7 @@ public class DozeOptimize extends CordovaPlugin {
 
                     callbackContext.success("requested");
                     return true;
-                }else{
+                } else {
                     callbackContext.error("DATA_SAVER Not available.");
                     return false;
                 }
@@ -178,5 +174,27 @@ public class DozeOptimize extends CordovaPlugin {
             }
         }
         return false;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == REQUEST_BATTERY_OPTIMIZATIONS) {
+            PluginResult resultado = new PluginResult(PluginResult.Status.OK, resultCode == 1 ? "true" : "false");
+            resultado.setKeepCallback(true);
+            PUBLIC_CALLBACKS.sendPluginResult(resultado);
+        }
+
+        // Handle other results if exists.
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    // A function to show a toast with some data, just demo
+    private void tolog(String toLog) {
+        Context context = cordova.getActivity();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, toLog, duration);
+        toast.show();
     }
 }
